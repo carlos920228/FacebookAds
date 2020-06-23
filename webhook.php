aee8a781257dd22117f624df4290892d4a8333ae
Carlos Humberto Romero Callejas <carlos920228@gmail.com>
2020-06-23 10:45:39 -0500

    Test email leads

--- webhook.php
+++ webhook.php
@@ -8,3 +8,12 @@ echo $challenge;
 
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
+ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "noreply@dannyyesoft.mx";
+    $to = "cromero@dannyyesoft.mx, carlos.romero@iuv.edu.mx";
+    $subject = "Tienes un nuevo lead";
+    $message = "Tienes un nuevo lead, por favor ve a tu portal de seguiento";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
