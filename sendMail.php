898f34749ff5589ee71bfa6f9aecc6621798baad
Carlos Humberto Romero Callejas <carlos920228@gmail.com>
2020-06-23 10:21:48 -0500

    Test email v1

--- sendMail.php
+++ sendMail.php
@@ -2,9 +2,9 @@
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
     $from = "noreply@dannyyesoft.mx";
-    $to = "cromero@dannyyesoft.mx";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
+    $to = "cromero@dannyyesoft.mx, carlos.romero@iuv.edu.mx";
+    $subject = "Tienes un nuevo lead";
+    $message = "Tienes un nuevo lead, por favor ve a tu portal de seguiento";
     $headers = "From:" . $from;
     mail($to,$subject,$message, $headers);
     echo "The email message was sent.";
